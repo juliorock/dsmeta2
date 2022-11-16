5c01a196c7befd955c13ea349bbbc7e5b7ebc820
julio cesar <julio.queiroga@hotmail.com>
2022-11-16 10:32:53 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,14 @@
+import NotificationButton from "./components/NotificationButton"
 
 function App() {
 
   return(
-
+   <>
     <h1>olá mundo</h1>
-  )
+    <NotificationButton />
   
+  </>
+  )
 }
 
 export default App
